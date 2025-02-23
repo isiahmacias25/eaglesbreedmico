128562d7695fd85a3473479514814051739a2aea
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-02-23 03:59:28 -0500

    Update indexHome.js

--- Assets/Js/indexHome.js
+++ Assets/Js/indexHome.js
@@ -1,4 +1,6 @@
 // Function to close the popup when the close button is clicked
 function ConstructionClosePopup() {
+  // Ensure the ID matches exactly with the HTML
   document.getElementById('constructionPopup').style.display = 'none';
 }
+
