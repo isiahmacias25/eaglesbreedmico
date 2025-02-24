20890709ed6d77cb48041c915100690a6084d973
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-02-24 15:43:52 -0500

    Create meetingMinutes.js

---
+++ Assets/Js/MembersPortal/meetingMinutes.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChVYbT54aRIbAHyy_HRsH7caRHyaZwWTA",
+  authDomain: "eaglesbreedmico.firebaseapp.com",
+  projectId: "eaglesbreedmico",
+  storageBucket: "eaglesbreedmico.firebasestorage.app",
+  messagingSenderId: "258146487149",
+  appId: "1:258146487149:web:c443a6f9af1c929cb6e864",
+  measurementId: "G-ZR1P59C7BP"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
