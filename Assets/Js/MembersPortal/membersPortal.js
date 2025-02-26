4f73dba176b616c4bcaaa6895091d30d3abbe0c3
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-02-26 09:07:58 -0500

    Update membersPortal.js

--- Assets/Js/MembersPortal/membersPortal.js
+++ Assets/Js/MembersPortal/membersPortal.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // Firebase configuration
