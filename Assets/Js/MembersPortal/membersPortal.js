fd04c490947b793c8d654d24ccebfc29614ff0c6
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-02-27 14:27:35 -0500

    Update membersPortal.js

--- Assets/Js/MembersPortal/membersPortal.js
+++ Assets/Js/MembersPortal/membersPortal.js
@@ -22,14 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const welcomeMessage = document.getElementById("welcomeMessage");
   const loginError = document.getElementById("loginError");
 
-    if (window.location.pathname.includes("membersPortal.html")) {
-      if (!loginForm || !membersContent || !welcomeMessage || !loginError) {
-        console.error("One or more elements are missing from the DOM.");
-        return;
-      }
+  if (window.location.pathname.includes("membersPortal.html")) {
+    if (!loginForm || !membersContent || !welcomeMessage || !loginError) {
+      console.error("One or more elements are missing from the DOM.");
+      return;
     }
- }
-
+  }
+});  
   // Handle login form submission
   loginForm.addEventListener("submit", async function (event) {
     event.preventDefault();
