6c11fcd9014a3ec0066479d785df47c7790e8bd5
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-05-12 08:53:54 -0400

    Update membersPortal.js

--- Assets/Js/MembersPortal/membersPortal.js
+++ Assets/Js/MembersPortal/membersPortal.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("username", username); // Renamed to 'username'
 
         console.log("Login successful. Redirecting to members portal...");
-        window.location.href = "../MembersPortal/membersPortal.html"; // Redirect to members portal page after successful login
+        window.location.href = "../../../MembersPortal/membersPortal.html"; // Redirect to members portal page after successful login
       } catch (error) {
         console.error("Login error:", error);
         let errorMessage = "An error occurred. Please try again.";
