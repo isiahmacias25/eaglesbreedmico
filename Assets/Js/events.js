a458aaad566e976a9ffdb432194af306febb9734
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-06-27 22:04:50 -0400

    Update events.js

--- Assets/Js/events.js
+++ Assets/Js/events.js
@@ -161,6 +161,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const app = initializeApp(firebaseConfig);
       const db = getFirestore(app);
+      firebase.firestoreInstance = db; // store globally
 
       const viewBtn = document.getElementById('viewEventBtn');
       const viewSelect = document.getElementById('viewEventSelector');
